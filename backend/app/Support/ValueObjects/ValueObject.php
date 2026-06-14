0b86fe14a34504cc96d870aa66079fe9ab46b1e1
Joe Cianflone <joe@cianflone.co>
2026-06-14 11:00:42 -0400

    create an env file generator

--- backend/app/Support/ValueObjects/ValueObject.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace App\Support\ValueObjects;
-
-abstract readonly class ValueObject
-{
-    abstract public function __toString(): string;
-    abstract public function equals(self $other): bool;
-}
